971f997c757c795111a25c9e96dc00e465783faf
Pietro Rea <pietro@sweetpeamobile.com>
2021-10-27 14:55:44 -0400

    Mac setup - hide OS upgrade notification

--- mac/mac-setup.sh
+++ mac/mac-setup.sh
@@ -3,15 +3,10 @@
 # Finder & file system
 
 defaults write com.apple.finder AppleShowAllFiles -bool true
-
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
 defaults write com.apple.finder ShowStatusBar -bool true
-
 defaults write com.apple.finder ShowPathbar -bool true
-
 defaults write com.app./le.finder FXEnableExtensionChangeWarning -bool false
-
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
 chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
@@ -19,3 +14,8 @@ chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
 # Xcode
 
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool YES
+
+# System Preferences
+
+## Diasble upgrade notification badge (needs `killall Dock`)
+defaults write com.apple.systempreferences AttentionPrefBundleIDs -bool false
